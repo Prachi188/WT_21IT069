772093f0227b6ba6ed18bf92c3e77fceca2c8c30
Prachi188 <117380921+Prachi188@users.noreply.github.com>
2023-01-11 14:05:16 +0530

    Lab-4

---
+++ Lab-4/js/demo.js
@@ -0,0 +1,20 @@
+function maruti(){
+    let maruti = {
+    modelname : "ALTO",
+    price : 250000,
+};
+document.getElementaryByID("msg").textContent = maruti.name;
+}
+function maruti(){}
+let audi = {
+    modelname : "AUDI",
+    price : 7550000,
+    
+    }
+};
+
+let benz = {
+    modelname : "MERCEDES",
+    price : 15000000,
+    
+};
